d4698361b83178079a9402ca1450842697e7e0e8
MK <102797277+etak-yram@users.noreply.github.com>
2022-11-05 15:55:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-
+"""
+imports
+"""
 from website import create_app
 
 
+"""
+initialise
+"""
 app = create_app()
 
 
+"""
+endpoints
+"""
+
+
+"""
+Run Flask
+"""
 if __name__ == '__main__':
     app.run(debug=True)
